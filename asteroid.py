fdf41f55bb01f06ad818e4940c94e0bb84784cdf
Recklezz Penguin <aproberge@protonmail.com>
2024-09-11 11:21:20 -0700

    small change to split method in asteroid class

--- asteroid.py
+++ asteroid.py
@@ -16,7 +16,7 @@ class Asteroid(CircleShape):
     def split(self):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
-            return "this was a small asteroid"
+            return
         else:
             rand_angle = random.uniform(20.0, 50.0)
             angle_1 = self.velocity.rotate(rand_angle)
